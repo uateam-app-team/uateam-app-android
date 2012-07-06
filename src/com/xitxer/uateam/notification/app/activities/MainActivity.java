1ea1c9a5392906b8fd80ea277b36ff446a0abbc0
voidset <zipu4.post@gmail.com>
2012-07-06 17:33:35 +0300

    add first activity

---
+++ src/com/xitxer/uateam/notification/app/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xitxer.uateam.notification.app.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.xitxer.uateam.notification.app.R;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		setContentView(R.layout.main);
+		super.onCreate(savedInstanceState);
+	}
+}
